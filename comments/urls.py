b142785d363d76c6cf16bdcc675e80f53d0afe36
Amankaium <kaium@list.ru>
2024-08-02 11:36:24 +0600

    add generics v1

---
+++ comments/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from comments.views import *
+
+
+urlpatterns = [
+    path('v1/list/', CommentList.as_view()),
+    path('v1/create/', CommentCreateAPIView.as_view()),
+    path('v1/detail/<int:pk>/', CommentDetailAPIView.as_view()),
+    path('v1/update/<int:pk>/', CommentUpdateAPIView.as_view()),
+    path('v1/delete/<int:pk>/', CommentDeleteAPIView.as_view()),
+]
--- socialnet/settings.py
+++ socialnet/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     
     'userapp',
     'posts',
+    'comments',
 ]
 
 MIDDLEWARE = [
