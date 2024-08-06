7929335065f6cf651d189140f65c0b86df455aa4
Amankaium <kaium@list.ru>
2024-08-06 11:38:22 +0600

    added publication admin

--- posts/admin.py
+++ posts/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 from posts.models import *
 
 
-admin.site.register(Publication)
+@admin.register(Publication)
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'title', 'author', 
+        'created_at', 'updated_at'
+    ]
+    list_filter = ['author', 'created_at']
+    search_fields = [
+        'title', 'content', 
+        'author__first_name', 'author__last_name',
+        'author__username',
+    ]
+    list_editable = ['title', 'author']
+    
+    
+
+
 admin.site.register(Like)
